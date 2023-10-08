229bf3bfa25a8d0fd29bc7a5f5ae9e4cc28358c6
Jin-Byun <jbyun10@my.bcit.ca>
2023-10-07 20:27:05 -0700

    change: convert json from api to message

--- COMP4537/labs/4/js/get.js
+++ COMP4537/labs/4/js/get.js
@@ -1,24 +1,26 @@
-import {ROOT, ENDPOINT, ONLY_ABC} from "./constants.js";
+import { ROOT, ENDPOINT, ONLY_ABC } from "./constants.js";
 
 async function getDefinition() {
-    const word = document.getElementById("word").value.trim();
-    if (word === "") {
-        alert("Require input in word.");
-        return;
-    }
-    if (!ONLY_ABC.test(word)) {
-        alert("Only alphabets for word.");
-        return;
-    }
-    const display = document.getElementById("display");
-    display.innerHTML = "";
-    const response = await fetch(`${ROOT}/${ENDPOINT}/?word=${word}`);
-    if (response.status === 200) {
-        const definition = await response.text();
-        display.innerText = `"${word}": ${definition}`;
-        return;
-    }
-    display.innerText = `Error: "${word}" not found.`;
+  const word = document.getElementById("word").value.trim();
+  if (word === "") {
+    alert("Require input in word.");
+    return;
+  }
+  if (!ONLY_ABC.test(word)) {
+    alert("Only alphabets for word.");
+    return;
+  }
+  const display = document.getElementById("display");
+  display.innerHTML = "";
+  const response = await fetch(`${ROOT}/${ENDPOINT}/?word=${word}`);
+  const { count, message: definition } = await response.json();
+  if (response.status === 200) {
+    display.innerText = `request #${count} "${word}": ${definition}`;
+    return;
+  }
+  display.innerText = `request #${count} Error: "${word}" not found.`;
 }
 
-document.getElementById("search").addEventListener("click", () => {getDefinition()});
+document.getElementById("search").addEventListener("click", () => {
+  getDefinition();
+});
--- COMP4537/labs/4/js/post.js
+++ COMP4537/labs/4/js/post.js
@@ -1,38 +1,40 @@
-import {ROOT, ENDPOINT, ONLY_ABC, ADD_BUTTON_TEXT} from "./constants.js";
+import { ROOT, ENDPOINT, ONLY_ABC, ADD_BUTTON_TEXT } from "./constants.js";
 
 function addDefinition() {
-    const term = document.getElementById("terminology").value.trim();
-    const definition = document.getElementById("definition").value.trim();
-    if (term === "" || definition === "") {
-        alert("Require input in both terminology and definition.");
-        return;
+  const term = document.getElementById("terminology").value.trim();
+  const definition = document.getElementById("definition").value.trim();
+  if (term === "" || definition === "") {
+    alert("Require input in both terminology and definition.");
+    return;
+  }
+  if (!ONLY_ABC.test(term)) {
+    alert("no number permitted in terminology.");
+    return;
+  }
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", `${ROOT}/${ENDPOINT}/`, true);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState !== 4) return;
+    const button = document.getElementById("add");
+    const { count, message } = JSON.parse(this.responseText);
+    button.innerText = `Request #${count} ${message}`;
+    if (this.status == 200) {
+      button.className = "success";
+    } else {
+      button.className = "fail";
     }
-    if (!ONLY_ABC.test(term)) {
-        alert("no number permitted in terminology.");
-        return;
-    }
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", `${ROOT}/${ENDPOINT}/`, true);
-    xhttp.onreadystatechange = function() {
-        if (this.readyState !== 4) return;
-        const button = document.getElementById("add");
-        if (this.status == 200) {
-           button.innerText = this.responseText;
-           button.className = "success";
-        } else {
-            button.innerText = this.responseText;
-            button.className = "fail";
-        }
-        setTimeout(() => {
-            if (this.status === 200) {
-                document.getElementById("terminology").value = "";
-                document.getElementById("definition").value = "";
-            }
-            button.className = "";
-            button.innerText = ADD_BUTTON_TEXT;
-        }, 3000);
-    };
-    xhttp.send(`word=${term}&definition=${definition}`);
+    setTimeout(() => {
+      if (this.status === 200) {
+        document.getElementById("terminology").value = "";
+        document.getElementById("definition").value = "";
+      }
+      button.className = "";
+      button.innerText = ADD_BUTTON_TEXT;
+    }, 3000);
+  };
+  xhttp.send(`word=${term}&definition=${definition}`);
 }
 
-document.getElementById("add").addEventListener("click", () => {addDefinition()});
+document.getElementById("add").addEventListener("click", () => {
+  addDefinition();
+});
